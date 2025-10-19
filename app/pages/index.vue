313e609b266be222bd6b5ab0b17b8f65a57192d4
bsotoudeh <bsotoudehnik@dataguard.de>
2025-10-19 19:31:57 +0330

    feat: remove extra comment

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -30,22 +30,6 @@ const selectedValue = ref('');
 const searchValue = ref('');
 const colorMode = useColorMode();
 
-// const allCountries: Country[] = [
-//   { name: 'France', code: 'FR', population: 67000000, region: 'Europe', capital: 'Paris' },
-//   { name: 'Germany', code: 'DE', population: 83000000, region: 'Europe', capital: 'Berlin' },
-//   { name: 'Russia', code: 'RU', population: 146000000, region: 'Europe', capital: 'Moscow' },
-//   { name: 'Singapore', code: 'SG', population: 5900000, region: 'Asia', capital: 'Singapore' },
-//   { name: 'Netherlands', code: 'NL', population: 17000000, region: 'Europe', capital: 'Amsterdam' },
-//   { name: 'Iran', code: 'IR', population: 83000000, region: 'Asia', capital: 'Tehran' },
-//   { name: 'United States', code: 'US', population: 331000000, region: 'Americas', capital: 'Washington, D.C.' },
-//   { name: 'Canada', code: 'CA', population: 38000000, region: 'Americas', capital: 'Ottawa' },
-//   { name: 'Australia', code: 'AU', population: 25000000, region: 'Oceania', capital: 'Canberra' },
-//   { name: 'New Zealand', code: 'NZ', population: 5000000, region: 'Oceania', capital: 'Wellington' },
-//   { name: 'South Africa', code: 'ZA', population: 60000000, region: 'Africa', capital: 'Pretoria' },
-//   { name: 'India', code: 'IN', population: 1380000000, region: 'Asia', capital: 'New Delhi' },
-//   { name: 'China', code: 'CN', population: 1400000000, region: 'Asia', capital: 'Beijing' },
-// ]
-
 const { data: allCountries, error } = await useAsyncData('allCountries', async () => {
   try {
     const res = await $fetch('https://restcountries.com/v3.1/all?fields=name,flags,continents,capital,population')
